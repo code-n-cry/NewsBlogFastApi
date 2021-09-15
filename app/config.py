3160f78b98283b7d8ed680bb51edf930da4eb570
Egor <61789375+code-n-cry@users.noreply.github.com>
2021-09-15 20:46:10 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ import os
 DATABASE_URL = os.environ.get('DATABASE_URL')
 engine = create_engine(DATABASE_URL, echo=False)
 database = Database(DATABASE_URL)
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3j5"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
